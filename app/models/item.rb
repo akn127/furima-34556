2c3e4231504fde08a02b181bf996e2e1718f522c
akn127 <sbf-neppie@outlook.jp>
2021-02-22 14:49:11 +0900

    ActiveHashの導入、実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_fee
+  belongs_to :area
+  belongs_to :days
 
   with_options presence: true do
     validates :item_name
@@ -10,5 +16,13 @@ class Item < ApplicationRecord
     validates :area_id
     validates :days_id
     validates :price
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :status_id
+    validates :delivery_fee_id
+    validates :area_id
+    validates :days_id
   end
 end
