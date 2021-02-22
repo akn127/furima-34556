e7f2dbacd394f58b2dacd7c87f7a04202254de37
akn127 <sbf-neppie@outlook.jp>
2021-02-22 16:30:14 +0900

    ログイン画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
   end
 
@@ -6,7 +7,7 @@ class ItemsController < ApplicationController
     if user_signed_in?
       @item = Item.new
     else
-      redirect_to root_path
+      redirect_to action: :index 
     end
   end
 
