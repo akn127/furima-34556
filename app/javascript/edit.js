ec8b34a63b7169172714f676139dacfcaa6af6d4
akn127 <sbf-neppie@outlook.jp>
2021-02-24 11:36:03 +0900

    editのルーティング、viewファイルの実装

---
+++ app/javascript/edit.js
@@ -0,0 +1,15 @@
+window.addEventListener("load",()=>{
+
+  const price = document.getElementById("item-price");
+  price.addEventListener("input",()=>{
+    const inputValue = price.value;
+
+  const addTaxDom = document.getElementById("add-tax-price")
+  addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
+
+  const profitAmount = document.getElementById("profit")
+    const value_result = inputValue * 0.1
+    console.log(value_result)
+    profitAmount.innerHTML = (Math.floor(inputValue - value_result));
+  })
+})
