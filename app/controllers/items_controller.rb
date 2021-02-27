28ec16404dc4bd13c089aa7b11714e9915af1442
akn127 <sbf-neppie@outlook.jp>
2021-02-27 10:47:34 +0900

    ログイン状態による購入ページの表示分岐の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,8 +51,11 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless @item.user_id == current_user.id
-      redirect_to action: :index
+    @item = Item.find(params[:id])
+    if @item.order.present?
+      redirect_to root_path
+    elsif @item.user_id != current_user.id
+      redirect_to root_path
     end
   end
 
